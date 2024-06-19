9b94db392ca423745afad673a54e2aadad7400a1
mauwqq <mauw0@protonmail.com>
2024-06-19 11:48:52 -0300

    solved two-sum

--- python/two_sum.py
+++ python/two_sum.py
@@ -1,29 +1,10 @@
 # https://leetcode.com/problems/two-sum/
 
 
-def twoSum(nums, target):
-    result = []
-    for i in nums:
-        for j in range(len(nums) - 1):
-            izq = j
-            der = j + 1
-            if nums[izq] + nums[der] == target:
-                return [izq, der]
-    return []
-
-
-def main():
-    nums = []
-    array = input("Insert numbers of list, separated by a space: ")
-    x = array.split()
-    for i in x:
-        try:
-            i = int(i)
-            nums.append(i)
-        except:
-            print("this is not a number")
-    target = int(input("insert target: "))
-    twoSum(nums, target)
-
-
-main()
+class Solution(object):
+    def twoSum(nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
