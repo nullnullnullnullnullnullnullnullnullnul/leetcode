801973ed333ccf5f371848c9465514fc4e182557
0x80-kernel <mauw0@protonmail.com>
2025-07-24 16:34:58 -0300

    Longest substring without repeating characters done

---
+++ c/substring_not_repeating_chars.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// lastI is ASCII table making record of chars seen
+// using sliding window algorithm
+int lengthOfLongestSubstring(char *s)
+{
+  int count = 0;
+  int start = 0;
+  int lastI[123];
+  for (int i = 0; i < 123; i++) {
+    lastI[i] = -1;
+  }
+  for (int e = 0; s[e] != '\0'; e++) {
+    char c = s[e];
+    if (lastI[(unsigned char)c] >= start) {
+      start = lastI[(unsigned char)c] + 1;
+    }
+    lastI[(unsigned char)c] = e;
+    int windowLen = e - start + 1;
+    if (windowLen > count) {
+      count = windowLen;
+    }
+  }
+  return count;
+}
+
+int main(void)
+{
+  char *s = " ";
+  int count = lengthOfLongestSubstring(s);
+  printf("%i", count);
+  return 0;
+}
